59633d7dc203dc5c8dc4f220ec09cd361340ac5f
Sujoy Sen <sujoysenjitu1994@gmail.com>
2025-04-27 17:29:32 +0530

    Hooks concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 function App() {
 
+  let [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  }
+
   return (
-    <p>Hello Test</p>
+    <>
+        <h2>Counter Test</h2>
+        <h4>Counter Value: {counter}</h4>
+        <button onClick={addValue}>Add Value</button>
+        <br /><br />
+        <button onClick={removeValue}>Remove Value</button>
+    </>
   )
 }
 
