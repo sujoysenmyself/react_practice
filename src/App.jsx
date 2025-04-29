d44ed00f184a1791d69a2d973dd3c73e72c870bb
Sujoy Sen <sujoysenjitu1994@gmail.com>
2025-04-29 17:00:27 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 
-  const [color, setColor] = useState("olive")
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("count value:", count);
+  }, [count]);
 
   return (
-    <>
-      <div className="w-full h-screen duration-200" style={{backgroundColor: color}}></div>
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-      <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-        <button onClick={() => setColor("red")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor: "red"}}>Red</button>
-        <button onClick={() => setColor("green")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor: "green"}}>Green</button>
-        <button onClick={() => setColor("blue")} className="outline-none px-4 rounded-full text-white shadow-lg" style={{backgroundColor: "blue"}}>Blue</button>
-      </div>
-   </div>
-    </>
+    <div className="container effect-container">
+      <h1>useEffect Hook</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
   );
 }
 
