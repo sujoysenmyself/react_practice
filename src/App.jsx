db3be25b4529e2de5fbd5f78608adcae3e905bb8
Sujoy Sen <sujoysenjitu1994@gmail.com>
2025-04-29 17:31:55 +0530

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState, useEffect } from "react";
+import { useRef } from "react";
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  const inputRef=useRef("Sujoy Sen");
 
-  useEffect(() => {
-    console.log("count value:", count);
-  }, [count]);
+  const inputHandler=()=>{
+    inputRef.current.focus()
+  }
 
   return (
-    <div className="container effect-container">
-      <h1>useEffect Hook</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold mb-6 text-blue-600">UseRef</h1>
+      <input ref={inputRef} type="text" placeholder="Enter User Name" className="border border-gray-300 px-4 py-2 w-64 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"  />
+      <button onClick={inputHandler} className="bg-blue-600 text-white px-6 py-2 hover:bg-blue-700 transition duration-300">Focus On Input Field</button>
+    </>
   );
 }
 
