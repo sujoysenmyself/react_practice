b050b1258a27dca5c4a92eb40e1ef89836a2d37d
Sujoy Sen <sujoysenjitu1994@gmail.com>
2025-04-30 16:43:01 +0530

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import CounterComponent from './components/CounterComponents';
+import { Route, Routes, Link } from "react-router";
+import Home from "./components/Home";
+import About from "./components/About";
+import PageNowFound from "./components/PageNowFound";
 
 function App() {
   return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>React Custom Hook Example</h1>
-      <CounterComponent />
-    </div>
+    <>
+
+    <Link to="/">Home</Link>
+    <br />
+    <Link to="/about">About</Link>
+    <br /><br />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<PageNowFound />} />
+        </Routes>
+    </>
   );
 }
 
---
+++ src/components/PageNowFound.jsx
@@ -0,0 +1,9 @@
+function PageNowFound() {
+    return (
+      <>
+          <h1>This is 404 Page Not Found</h1>
+      </>
+    );
+  }
+  
+  export default PageNowFound;
